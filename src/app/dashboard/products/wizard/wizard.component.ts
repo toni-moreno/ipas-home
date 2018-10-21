03aa9f2161d9679f2cb8bf1d901ab3a95c582dea
sbengo <serigna@gmail.com>
2018-10-21 18:59:50 +0200

    improved product component and new product wizard

--- src/app/dashboard/products/wizard/wizard.component.ts
+++ src/app/dashboard/products/wizard/wizard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { ProductSample } from '../product.data';
-import { WizardService } from  './wizard.service';
+import { WizardService } from './wizard.service';
 
 import * as _ from 'yamljs';
 
@@ -76,71 +76,71 @@ export const EngineSNMPParams = {
       {
         key: "PRODUCT_CONCURRENTGATHER",
         agent_key: "ConcurrentGather",
-        description: 'OpenanewSNMPConnectionforeachmeasurementandsendconcurrentqueriesoverthedevice',
+        description: 'Open a new SNMP Connection foreach measurement and send concurrent queries over the device',
         type: "boolean",
         value: true
       }
     ],
-    platform_params:
-     [
+  platform_params:
+    [
       {
         key: "PLATFORM_INFLUXSERVER",
         agent_key: "OutDB",
-        description: 'InfluxDBserverIDtosenddatato',
+        description: 'InfluxDB server ID to send data to',
         type: "string",
         value: "default"
       },
       {
         key: "PLATFORM_LOGLEVEL",
         agent_key: "LogLevel",
-        description: 'Enableuseselectverbosityofthedevicelog',
+        description: 'Enable use select verbosity of the device log',
         type: "string",
         value: "info"
       },
       {
         key: "PLATFORM_GATHER_FREQ",
         agent_key: "Freq",
-        description: 'Frequencyofpollinginseconds(default30secifnotset)',
+        description: 'Frequency of polling in seconds (default 30 sec if not set)',
         type: "string",
         value: 60
       },
       {
         key: "PLATFORM_TIMEOUT",
         agent_key: "Timeout",
-        description: "TimeoutforeachSNMPquery",
+        description: "Timeout for each SNMP query",
         type: "integer",
         value: 20
       },
       {
         key: "PLATFORM_RETRIES",
         agent_key: "Retries",
-        description: 'SetsthenumberofretriestoattemptwithinTimeout',
+        description: 'Sets the number of retries to attempt within Timeout',
         type: "integer",
         value: 5
       },
       {
         key: "PLATFORM_UPDATE_FILTER_PREQ",
         agent_key: "UpdateFltFreq",
-        description: 'Numberpofcompletepollsthatagentwillwaitbeforeperformsnmptableindex/filterupdates.ThefinalupdatetimewillbeFreq*FilterUpdate(inseconds)Default60(1helapsedtime)',
+        description: 'Number pof complete polls that agent will wait before perform snmp table index/filter updates. The final update time will be Freq*FilterUpdate (in seconds) Default 60 (1h elapsed time)',
         type: "integer",
         value: 60
       },
       {
         key: "PLATFORM_SNMPDEBUG",
         agent_key: "SnmpDebug",
-        description: 'Ifthisoptionissetallsnmplowlevelprotocolquerieswithdetailedinfoonthisdevicewillbewrittenonafilewithname"snmpdebug_XXXXXX.log"',
+        description: 'If this option is set all snmp low level protocol queries with detailed info on this device will be written on a file with name "snmpdebug_XXXXXX.log"',
         type: "boolean",
         value: false
       },
       {
         key: "PLATFORM_EXTRATAG_KEYS",
         agent_key: "ExtraTags",
-        description: 'DeviceExtraTagkeys',
+        description: 'Device ExtraTag keys',
         type: "string",
         value: null
       }
     ],
-    device_params:
+  device_params:
     [
       {
         key: "DEVICE_ID",
@@ -247,7 +247,7 @@ export const EngineSNMPParams = {
         type: "string",
         value: null
       }
-  ]
+    ]
 }
 
 
@@ -272,9 +272,10 @@ export class WizardComponent implements OnInit {
   productFormGroup: any;
 
   gather_engines: Engine[] = [
-    {name: 'SNMP', type: 'snmpcollector'},
-    {name: 'Telegraf', type: 'telegraf'},
-    {name: 'SQLCollector', type: 'sqlcollector'}
+    { name: 'SNMP', type: 'snmpcollector' },
+    { name: 'Telegraf', type: 'telegraf' },
+    { name: 'SQLCollector', type: 'sqlcollector' },
+    { name: 'External', type: 'external' }
   ];
 
   visual_engines: Engine[] = [
@@ -286,7 +287,7 @@ export class WizardComponent implements OnInit {
     { name: 'Resistor', type: 'resistor' }
   ];
 
-  constructor(private _formBuilder: FormBuilder, public wizardService : WizardService) { }
+  constructor(private _formBuilder: FormBuilder, public wizardService: WizardService) { }
 
   //Set core config vars as formarrays, it will be easier to go over them
 
@@ -310,12 +311,26 @@ export class WizardComponent implements OnInit {
 
   //Generic function to add a engine on every step - 'gather',  'visual', 'alert'
   addEngine(step: string, engine: string) {
+    console.log("STEPPP", step);
+    console.log("STEPPP", engine);
     //console.log(this.productFormGroup.controls[step].value.length);
-      if (!this.productFormGroup.controls[step].value.find((element)=> {console.log(element); return element.engine === engine})) {
-        this.gather.push(this.createEngineFormGroup(engine));
+    if (engine != 'external') {
+      if (!this.productFormGroup.controls[step].value.find((element) => { console.log(element); return element.engine === engine })) {
+        switch (step) {
+          case 'gather':
+            this.gather.push(this.createEngineFormGroup(engine));
+            break;
+          case 'visual':
+            this.visual.push(this.createEngineFormGroup(engine));
+            break;
+          case 'alert':
+            this.alert.push(this.createEngineFormGroup(engine));
+            break;
+        }
       } else {
-        alert('Engine '+ engine +' is already defined on '+ step)
+        alert('Engine ' + engine + ' is already defined on ' + step)
       }
+    }
   }
 
   //Deletes an engine, it is required the step - 'gather', 'visual', 'alert' and the array index
@@ -342,21 +357,22 @@ export class WizardComponent implements OnInit {
   }
 
   //Deletes a config engine based on step, i, and  p
-  deleteConfigEngine(step : string, iengine : number, iconfig: number) {
+  deleteConfigEngine(step: string, iengine: number, iconfig: number) {
     console.log(step, iengine, iconfig);
     this.productFormGroup.controls[step].controls[iengine].controls.config.removeAt(iconfig)
   }
 
   //Creates a for for each configuration with params coming from the engine
-  createConfigFromEngine(engine : string): AbstractControl {
+  createConfigFromEngine(engine: string): AbstractControl {
     return this._formBuilder.group({
       name: ['', Validators.required],
       models: ['', Validators.required],
       description: ['', Validators.required],
       dir: ['', Validators.required],
       config: [''],
-      params: new FormGroup (
-        { 'product_params': this.createParamsFromEngine(engine, 'product_params'),
+      params: new FormGroup(
+        {
+          'product_params': this.createParamsFromEngine(engine, 'product_params'),
           'platform_params': this.createParamsFromEngine(engine, 'platform_params')
           //'device_params': this.createParamsFromEngine(engine, 'device_params')
         })
@@ -386,7 +402,7 @@ export class WizardComponent implements OnInit {
 
   testYAML() {
     let nativeObject = _.parse('hola: e');
-    console.log("NATIVE",nativeObject);
+    console.log("NATIVE", nativeObject);
 
     this.wizardService.retrieveYAML('https://gist.githubusercontent.com/chriscowley/8598119/raw/8f671464f914320281e5e75bb8dcbe11285d21e6/nfs.example.lan.yml').subscribe(
       data => {
@@ -394,20 +410,20 @@ export class WizardComponent implements OnInit {
         console.log(nativeObject);
       },
       err => console.error(err),
-      ()  => console.log("DON")
+      () => console.log("DON")
     )
   }
 
- createNewProduct() {
-  let yamlString = _.stringify(this.productFormGroup.value,999)
-  console.log(yamlString);
-   this.wizardService.createNewProduct('http://localhost:4200', _.stringify(this.productFormGroup.value,999,2))
-   .subscribe(
-     data => console.log(data),
-     err => console.error(err),
-     () => console.log("OK, DONE")
-   )
- }
+  createNewProduct() {
+    let yamlString = _.stringify(this.productFormGroup.value, 999)
+    console.log(yamlString);
+    this.wizardService.createNewProduct('http://localhost:4200', _.stringify(this.productFormGroup.value, 999, 2))
+      .subscribe(
+      data => console.log(data),
+      err => console.error(err),
+      () => console.log("OK, DONE")
+      )
+  }
 
 
 }
